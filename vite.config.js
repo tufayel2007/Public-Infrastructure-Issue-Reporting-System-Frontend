c60e8cc0846ee5493ccbf34c317644a76f93ca2f
tufayel2007 <ta07092007@gmail.com>
2025-12-15 15:37:08 +0600

    Update: All Issues page & issue card U I update vite config files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+// https://vite.dev/con
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
