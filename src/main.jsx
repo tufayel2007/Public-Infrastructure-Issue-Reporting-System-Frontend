b2edad25dfb58069e6a625d204b15eda0e990327
tufayel2007 <ta07092007@gmail.com>
2025-12-15 15:43:39 +0600

    update  main .jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./context/AuthContext";
 
 const queryClient = new QueryClient();
-
+// sdkfolkodsj
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
